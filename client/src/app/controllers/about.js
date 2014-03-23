66be4fb023902698ea463c561f28ec126c80e341
armandorvila <armando.ramirez.vila@gmail.com>
2014-03-23 17:23:14 +0100

    About page

--- client/src/app/controllers/about.js
+++ client/src/app/controllers/about.js
@@ -2,7 +2,7 @@ var about = angular.module('controllers.about', []);
 
 about.controller('AboutCtrl',['$scope','$rootScope', function($scope, $rootScope) {
     
-    $rootScope.subheader.title = 'Ablout Geodecisions';
+    $rootScope.subheader.title = 'About Geodecisions';
     $rootScope.subheader.description = 'What is Geodecisions intended for? Can I take advantage of Geodecisions?';
     
 		
